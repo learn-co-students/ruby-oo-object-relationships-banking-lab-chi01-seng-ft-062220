6afe5e93e8810c851361ee4849c45f60c869c647
Eve Reichmann <eve.reichmann@gmail.com>
2020-06-26 20:32:35 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
   # your code here
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status
+  @@all = []
+  def initialize(sender, receiver, amount)
+    @amount = amount
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def valid?
+    self.sender.valid? && self.receiver.valid?   
+  end  
+  def execute_transaction    
+  end    
 end
